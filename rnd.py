e9b88f59afb3877bc068666883c17968c274e279
btapple <bt_apple@qq.com>
2018-03-15 00:11:48 +0800

    占坑

---
+++ rnd.py
@@ -0,0 +1,18 @@
+import random
+import json
+import bisect
+# 实现：将权重加起来，在[0,weights_all]之间进行随机
+
+class Rnd:
+    AllWeights=0
+
+    def __init__(self):
+        # with open("./food.json","r+") as json_file:
+        #     self.json_data=json.load(json_file)
+            
+    def GetAllWeights(self):
+        pass
+    
+    def WeightedRandom():
+        pass
+
