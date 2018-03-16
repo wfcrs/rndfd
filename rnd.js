eecdbdf0920989d2ba0455ec943238b25031bd77
Lynz Rand <plok314159265@163.com>
2018-03-16 16:53:59 +0800

    add basic food.json

--- rnd.js
+++ rnd.js
@@ -1,17 +1,20 @@
-const foods = {
-  // foodname: weight
-  shuijiao: 1,
-  tieban: 3,
-  erlou: 3,
-  jipaifan: 3
-}
+// const foods = {
+//   // foodname: weight
+//   shuijiao: 1,
+//   tieban: 3,
+//   erlou: 3,
+//   jipaifan: 3
+// }
 
-const dict = {
-  shuijiao: '水饺',
-  tieban: '铁板',
-  erlou: '二楼',
-  jipaifan: '鸡排饭'
-}
+// const dict = {
+//   // foodname: dictionary
+//   shuijiao: '水饺',
+//   tieban: '铁板',
+//   erlou: '二楼',
+//   jipaifan: '鸡排饭'
+// }
+
+let json = ""
 
 let sum = 0
 Object.values(foods).map((v, i) => (sum += v))
