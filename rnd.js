9a38cfa473d2af6b16cd7ad02fb170ab8cc3ec2f
Lynz Rand <plok314159265@163.com>
2018-03-16 17:14:00 +0800

    .

--- rnd.js
+++ rnd.js
@@ -14,25 +14,36 @@
 //   jipaifan: '鸡排饭'
 // }
 
-let json = ""
+let jsondir = "https://github.com/wfcrs/rndfd/raw/master/foods.json"
+
+function getKeys() {
+  var xmlhttp;
+  if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
+  else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  xmlhttp.open("GET", jsondir, false);
+  xmlhttp.send();
+  return JSON.parse(xmlhttp.responseText)
+}
 
 let sum = 0
-Object.values(foods).map((v, i) => (sum += v))
+
+let foods = getKeys();
+Object.keys(foods).map((v, i) => (sum += foods[v].weight))
 console.log(
   'Array contains',
-  Object.values(foods).length,
+  Object.keys(foods).length,
   'foods. Weight sum is',
   sum
 )
 let r = Math.random() * sum
-console.log('r =',r)
+console.log('r =', r)
 let selectedFood
 for (let food of Object.keys(foods)) {
   r -= foods[food]
   if (r <= 0) {
-    selectedFood = dict[food]
+    selectedFood = foods[food].name
     break
   }
 }
 console.log('--------\n')
-console.log(selectedFood,'\n\n')
+console.log(selectedFood, '\n\n')
